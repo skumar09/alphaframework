4efb68cf7208afbe38c01894d533b524e776a5c5
nateekar <nateekar@adobe.com>
2023-09-23 13:44:02 -0700

    update-5

--- install.mjs
+++ install.mjs
@@ -1,40 +1,49 @@
+// import and alias fs.promises
 import inquirer from 'inquirer';
-import fs from 'fs';
+import { promises as fsPromises } from 'fs';
 import path from 'path';
-import { promisify } from 'util';
 
-const copyFile = promisify(fs.copyFile);
+const { copyFile, mkdir, readdir } = fsPromises;
 
 const filesToCopy = [
   {
-    source: path.join(__dirname, '.github', 'workflows', 'dailyrun.yml'),
+    source: path.join(import.meta.url, '.github', 'workflows', 'dailyrun.yml'),
     target: path.join(process.cwd(), '.github', 'workflows', 'dailyrun.yml'),
   },
   {
-    source: path.join(__dirname, '.github', 'workflows', 'selfrun.yml'),
+    source: path.join(import.meta.url, '.github', 'workflows', 'selfrun.yml'),
     target: path.join(process.cwd(), '.github', 'workflows', 'selfrun.yml'),
   },
   {
-    source: path.join(__dirname, 'selectors', 'example.block.js'),
+    source: path.join(import.meta.url, 'selectors', 'example.block.js'),
     target: path.join(process.cwd(), 'selectors', 'example.block.js'),
   },
   {
-    source: path.join(__dirname, 'features', 'example.spec.js'),
+    source: path.join(import.meta.url, 'features', 'example.spec.js'),
     target: path.join(process.cwd(), 'features', 'example.spec.js'),
   },
   {
-    source: path.join(__dirname, 'tests', 'example.test.js'),
+    source: path.join(import.meta.url, 'tests', 'example.test.js'),
     target: path.join(process.cwd(), 'tests', 'example.test.js'),
   },
   {
-    source: path.join(__dirname, 'global.setup.js'),
+    source: path.join(import.meta.url, 'global.setup.js'),
     target: path.join(process.cwd(), 'global.setup.js'),
   },
   {
-    source: path.join(__dirname, 'playwright.config.js'),
+    source: path.join(import.meta.url, 'playwright.config.js'),
     target: path.join(process.cwd(), 'playwright.config.js'),
   },
 ];
+
+async function copyFileIfExists(source, target) {
+  try {
+    await copyFile(source, target);
+    console.log(`Copied ${source} to ${target}`);
+  } catch (error) {
+    console.error(`Error copying ${source} to ${target}: ${error.message}`);
+  }
+}
 
 // Prompt the user about GitHub Actions and necessary folders
 inquirer
@@ -56,14 +65,8 @@ inquirer
     if (answers.includeGitHubActions) {
       // Copy GitHub Action files to the project directory
       for (const { source, target } of filesToCopy.slice(0, 2)) {
-        // Check if the target file already exists
-        if (!fs.existsSync(target)) {
-          try {
-            await copyFile(source, target);
-            console.log(`Copied ${source} to ${target}`);
-          } catch (error) {
-            console.error(`Error copying ${source} to ${target}: ${error.message}`);
-          }
+        if (!(await fileExists(target))) {
+          await copyFileIfExists(source, target);
         } else {
           console.log(`Skipped copying ${source} to ${target} (already exists).`);
         }
@@ -77,39 +80,26 @@ inquirer
       for (const { source, target } of filesToCopy.slice(2)) {
         const folderPath = path.dirname(target);
 
-        // Check if the target folder already exists
-        if (!fs.existsSync(folderPath)) {
-          fs.mkdirSync(folderPath, { recursive: true });
+        if (!(await folderExists(folderPath))) {
+          await mkdir(folderPath, { recursive: true });
         }
 
-        // Check if the target file already exists
-        if (!fs.existsSync(target)) {
-          try {
-            await copyFile(source, target);
-            console.log(`Copied ${source} to ${target}`);
-          } catch (error) {
-            console.error(`Error copying ${source} to ${target}: ${error.message}`);
-          }
+        if (!(await fileExists(target))) {
+          await copyFileIfExists(source, target);
         } else {
           console.log(`Skipped copying ${source} to ${target} (already exists).`);
         }
       }
-      
+
       // Copy global.setup.js and playwright.config.js to the root folder
       for (const { source, target } of filesToCopy.slice(5, 7)) {
-        // Check if the target file already exists
-        if (!fs.existsSync(target)) {
-          try {
-            await copyFile(source, target);
-            console.log(`Copied ${source} to ${target}`);
-          } catch (error) {
-            console.error(`Error copying ${source} to ${target}: ${error.message}`);
-          }
+        if (!(await fileExists(target))) {
+          await copyFileIfExists(source, target);
         } else {
           console.log(`Skipped copying ${source} to ${target} (already exists).`);
         }
       }
-      
+
       console.log('Necessary folders and files have been added to your project.');
     } else {
       console.log('Necessary folders and files were not added to your project.');
@@ -117,3 +107,21 @@ inquirer
 
     // Continue with the rest of your installation logic...
   });
+
+async function fileExists(filePath) {
+  try {
+    await fsPromises.access(filePath);
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
+
+async function folderExists(folderPath) {
+  try {
+    const items = await readdir(folderPath);
+    return items && items.length > 0;
+  } catch (error) {
+    return false;
+  }
+}
