21344d3926b743f2a2bd7bb177e278006119f364
nateekar <nateekar@adobe.com>
2023-09-23 16:43:23 -0700

    update-17

--- install.mjs
+++ install.mjs
@@ -1,38 +1,17 @@
-import inquirer from 'inquirer';
+// postinstall.mjs
+
 import { promises as fsPromises } from 'fs';
 import path from 'path';
+import inquirer from 'inquirer';
 
-const { copyFile, mkdir, readdir } = fsPromises;
+const { copyFile, mkdir, readdir, access } = fsPromises;
 
 const filesToCopy = [
   {
-    source: new URL('.github/workflows/dailyrun.yml', import.meta.url),
+    source: path.join(__dirname, '.github', 'workflows', 'dailyrun.yml'),
     target: path.join(process.cwd(), '.github', 'workflows', 'dailyrun.yml'),
   },
-  {
-    source: new URL('.github/workflows/selfrun.yml', import.meta.url),
-    target: path.join(process.cwd(), '.github', 'workflows', 'selfrun.yml'),
-  },
-  {
-    source: new URL('selectors/example.block.js', import.meta.url),
-    target: path.join(process.cwd(), 'selectors', 'example.block.js'),
-  },
-  {
-    source: new URL('features/example.spec.js', import.meta.url),
-    target: path.join(process.cwd(), 'features', 'example.spec.js'),
-  },
-  {
-    source: new URL('tests/example.test.js', import.meta.url),
-    target: path.join(process.cwd(), 'tests', 'example.test.js'),
-  },
-  {
-    source: new URL('global.setup.js', import.meta.url),
-    target: path.join(process.cwd(), 'global.setup.js'),
-  },
-  {
-    source: new URL('playwright.config.js', import.meta.url),
-    target: path.join(process.cwd(), 'playwright.config.js'),
-  },
+  // Add more files to copy here if needed
 ];
 
 async function copyFileIfExists(source, target) {
@@ -44,32 +23,29 @@ async function copyFileIfExists(source, target) {
   }
 }
 
-// Prompt the user about GitHub Actions and necessary folders
-inquirer
-  .prompt([
-    {
-      type: 'confirm',
-      name: 'includeGitHubActions',
-      message: 'Do you want to include GitHub Actions files?',
-      default: false,
-    },
-    {
-      type: 'confirm',
-      name: 'createFolders',
-      message: 'Do you want to create the necessary folders (selectors, features, tests)?',
-      default: true,
-    },
-  ])
-  .then(async (answers) => {
+async function setup() {
+  try {
+    // Prompt the user about GitHub Actions and necessary folders
+    const answers = await inquirer.prompt([
+      {
+        type: 'confirm',
+        name: 'includeGitHubActions',
+        message: 'Do you want to include GitHub Actions files?',
+        default: false,
+      },
+      {
+        type: 'confirm',
+        name: 'createFolders',
+        message: 'Do you want to create the necessary folders (selectors, features, tests)?',
+        default: true,
+      },
+    ]);
+
     if (answers.includeGitHubActions) {
       // Copy GitHub Action files to the project directory
-      for (const { source, target } of filesToCopy.slice(0, 2)) {
+      for (const { source, target } of filesToCopy) {
         if (!(await fileExists(target))) {
-          try {
-            await copyFileIfExists(source, target);
-          } catch (error) {
-            console.error(`Error copying ${source} to ${target}: ${error.message}`);
-          }
+          await copyFileIfExists(source, target);
         } else {
           console.log(`Skipped copying ${source} to ${target} (already exists).`);
         }
@@ -80,63 +56,34 @@ inquirer
 
     if (answers.createFolders) {
       // Create the necessary folders
-      for (const { source, target } of filesToCopy.slice(2)) {
-        const folderPath = path.dirname(target);
+      const foldersToCreate = [
+        path.join(process.cwd(), 'selectors'),
+        path.join(process.cwd(), 'features'),
+        path.join(process.cwd(), 'tests'),
+      ];
 
-        if (!(await folderExists(folderPath))) {
-          try {
-            await mkdir(folderPath, { recursive: true });
-          } catch (error) {
-            console.error(`Error creating folder ${folderPath}: ${error.message}`);
-          }
-        }
-
-        if (!(await fileExists(target))) {
-          try {
-            await copyFileIfExists(source, target);
-          } catch (error) {
-            console.error(`Error copying ${source} to ${target}: ${error.message}`);
-          }
-        } else {
-          console.log(`Skipped copying ${source} to ${target} (already exists).`);
-        }
+      for (const folder of foldersToCreate) {
+        await mkdir(folder, { recursive: true });
       }
 
-      // Copy global.setup.js and playwright.config.js to the root folder
-      for (const { source, target } of filesToCopy.slice(5, 7)) {
-        if (!(await fileExists(target))) {
-          try {
-            await copyFileIfExists(source, target);
-          } catch (error) {
-            console.error(`Error copying ${source} to ${target}: ${error.message}`);
-          }
-        } else {
-          console.log(`Skipped copying ${source} to ${target} (already exists).`);
-        }
-      }
-
-      console.log('Necessary folders and files have been added to your project.');
+      console.log('Necessary folders have been added to your project.');
     } else {
-      console.log('Necessary folders and files were not added to your project.');
+      console.log('Necessary folders were not added to your project.');
     }
 
-    // Continue with the rest of your installation logic...
-  });
+    console.log('Post-installation setup completed.');
+  } catch (error) {
+    console.error('Error during post-installation setup:', error.message);
+  }
+}
 
 async function fileExists(filePath) {
   try {
-    await fsPromises.access(filePath);
+    await access(filePath);
     return true;
   } catch (error) {
     return false;
   }
 }
 
-async function folderExists(folderPath) {
-  try {
-    const items = await readdir(folderPath);
-    return items && items.length > 0;
-  } catch (error) {
-    return false;
-  }
-}
+setup();
